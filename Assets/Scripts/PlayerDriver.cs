2593f198c071388104c24bc613bbf86a67722908
Hannah R. <hannahsignupapps@gmail.com>
2025-09-11 16:30:15 -0700

    Clamped Velocity

--- Assets/Scripts/PlayerDriver.cs
+++ Assets/Scripts/PlayerDriver.cs
@@ -9,12 +9,16 @@ public class PlayerDriver : MonoBehaviour {
 
     public float horizontalSpeed = 10f;
     public float veritcalSpeed = -10f;
+    public float horizontalAccelerationSpeed = 80f;
+    public float verticalAccelerationSpeed = -9.8f;
 
     [Header("Pushback")]
     public float pushBackSpeed = 20f;
     public float pushBackDampSpeed = 40f;
 
+    public Vector3 velocity;
     public Vector3 pushBackVelocity;
+    //public Vector3 clampAcceleration;
 
     private void Awake() {
         Instance = this;
@@ -24,9 +28,12 @@ public class PlayerDriver : MonoBehaviour {
     void Update() {
         var i = GetInput();
 
+        velocity += new Vector3(i.horizontal * horizontalAccelerationSpeed, verticalAccelerationSpeed) * Time.deltaTime;
+        velocity.y = Mathf.Clamp(velocity.y, veritcalSpeed, 0);
+        Debug.Log(velocity);
         // this is the offset that we will move the player every frame
         // we scale the offset by Time.deltaTime so the offset is consistent every frame
-        var offset = new Vector3(i.horizontal * horizontalSpeed, veritcalSpeed) * Time.deltaTime;
+        var offset = velocity * Time.deltaTime;
 
         offset += pushBackVelocity * Time.deltaTime;
         pushBackVelocity = Vector3.MoveTowards(pushBackVelocity, Vector3.zero, Time.deltaTime * pushBackDampSpeed);
