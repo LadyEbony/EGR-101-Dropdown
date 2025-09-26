abba0b894b75e7e37c7018d315654deb5d3871cd
SwiggedySpoof <tjalexg69@gmail.com>
2025-09-26 11:53:39 -0700

    Made projectile destroy self after 5 seconds
    I made the projectile destroy itself after 5 seconds to avoid having a bunch of unnecessary data.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,6 +8,8 @@ public class Projectile : MonoBehaviour
     [SerializeField] private Rigidbody rb;
 
     [SerializeField] private float LaunchForce = 50f;
+
+    [SerializeField] private float destroyAfterSeconds = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,6 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Destroy(gameObject, destroyAfterSeconds); 
     }
 }
