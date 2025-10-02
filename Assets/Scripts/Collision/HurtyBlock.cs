9b48d91de3c0c59c0dc6dad139a7dba8a3c9b6b4
MASAMADRE\sandr <josegarcia8064@gmail.com>
2025-10-02 07:55:52 -0700

    added lava block

--- Assets/Scripts/Collision/HurtyBlock.cs
+++ Assets/Scripts/Collision/HurtyBlock.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HurtyBlock : CollisionBlock {
+
+    public GameObject bouncePrefab;
+
     public override void OnCollisionEnterPlayer(PlayerDriver player, ContactPoint cp)
     {
         player.Damage();
+
+        var project = Vector3.ProjectOnPlane(-cp.normal, Vector3.forward);
+        var copy = Instantiate(bouncePrefab, cp.point, Quaternion.LookRotation(project));
+        Destroy(copy, 5f);
     }
 }
